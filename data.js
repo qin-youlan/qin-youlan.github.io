80ff0f5d64ae5daeb8f1aeaea56755d076cf3782
qinyoulan <77440361+qin-youlan@users.noreply.github.com>
2025-10-18 23:38:49 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,32 +1,38 @@
-// data.js 文件，存储软件列表数据
 export const softwareList = [
     {
+        name: '诸神工具',
+        category: '游戏辅助工具',
+        icon: 'https://p11-flow-imagex-download-sign.byteimg.com/tos-cn-i-a9rns2rl98/81b375e1133349889e90a439080d55c2.png~tplv-a9rns2rl98-24:720:720.png?rcl=202510182304409CC5BC0D155843F79DA5&rk3s=8e244e95&rrcfp=8a172a1a&x-expires=1761404680&x-signature=XDk26f2c1cxtxgFZTEmgeuxSk3g%3D',
+        url: '/ldzs.html',
+        recommendText: '诸神皇冠辅助工具'
+    },
+ {
         name: 'Visual Studio',
         category: '开发工具',
-        icon: 'icons/vscode_icon.png',
+        icon: 'https://p3-flow-imagex-download-sign.byteimg.com/tos-cn-i-a9rns2rl98/644eeb4f229049cb8512ee56450a9984.png~tplv-a9rns2rl98-24:720:720.png?rcl=20251018230013E91D84754068C3CEBAA6&rk3s=8e244e95&rrcfp=8a172a1a&x-expires=1761404413&x-signature=OV2D%2F4R9cC0n9xFwlSJ0%2BOX36H4%3D',
         url: 'https://visualstudio.microsoft.com/zh-hans/',
         recommendText: '高效编码，必备工具！'
     },
     {
         name: 'Kimi.ai',
         category: '网站',
-        icon: 'icons/kimi_icon.png',
+        icon: 'https://p3-flow-imagex-download-sign.byteimg.com/tos-cn-i-a9rns2rl98/01ff3c446f704fa08ac980783b3fbdcc.png~tplv-a9rns2rl98-24:720:720.png?rcl=20251018230013E91D84754068C3CEBAA6&rk3s=8e244e95&rrcfp=8a172a1a&x-expires=1761404413&x-signature=Svn%2FZfTjGNco9057av8oihDrvgM%3D',
         url: 'https://kimi.moonshot.cn/',
         recommendText: 'Kimi智能助手'
     },
     {
         name: 'Via',
         category: '浏览器',
-        icon: 'icons/via_icon.png',
+        icon: 'https://p3-flow-imagex-download-sign.byteimg.com/tos-cn-i-a9rns2rl98/b55b2c2668984cf7aaba3eb500e13638.png~tplv-a9rns2rl98-24:720:720.png?rcl=20251018230013E91D84754068C3CEBAA6&rk3s=8e244e95&rrcfp=8a172a1a&x-expires=1761404413&x-signature=roNdq4WLmhR2juOx%2BOUxTE4Ujd8%3D',
         url: 'https://viayoo.com/zh-cn/',
         recommendText: '探索速度与简约的极限：似乎不凡的速度总会为无所不能的定制让步，似乎简约的界面总会为繁杂的功能牺牲，Via 浏览器在打破陈规旧则，让你看到另种可能。'
     },
     {
         name: 'FreeConvert',
         category: '网站',
-        icon: 'icons/FreeConvert_icon.png',
+        icon: 'https://p3-flow-imagex-download-sign.byteimg.com/tos-cn-i-a9rns2rl98/102b65ab58974502981514a8ee1a446c.png~tplv-a9rns2rl98-24:720:720.png?rcl=20251018230013E91D84754068C3CEBAA6&rk3s=8e244e95&rrcfp=8a172a1a&x-expires=1761404413&x-signature=qY6FHSJn88pywRgMsbCe1mpj7sk%3D',
         url: 'https://www.freeconvert.com/',
-        recommendText: '在线格式转换'
+        recommendText: '在线线格式转换'
     },
     {
         name: '花瓣网',
@@ -34,6 +40,6 @@ export const softwareList = [
         icon: 'icons/huaban.svg',
         url: 'https://huaban.com/',
         recommendText: '发现素材，探索灵感'
-    },
-    // 可以继续添加更多软件信息
+    }
+   
 ];
